822ae5a38a24b18c8250c4fa59f162d1f8f424b8
Rafael Pianaro <rafaelpianaro@gmail.com>
2023-05-16 22:19:09 -0300

    fix: index list of supports

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -13,11 +13,11 @@
     <tbody>
         @foreach ($supports as $support)
             <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ $support->status }}</td>
-                <td>{{ $support->body }}</td>
-                <td><a href="{{ route('supports.show', $support->id) }}">Visualizar</a></td>
-                <td><a href="{{ route('supports.edit', $support->id) }}">Editar</a></td>
+                <td>{{ $support['subject'] }}</td>
+                <td>{{ $support['status'] }}</td>
+                <td>{{ $support['body'] }}</td>
+                <td><a href="{{ route('supports.show', $support['id']) }}">Visualizar</a></td>
+                <td><a href="{{ route('supports.edit', $support['id']) }}">Editar</a></td>
             </tr>
         @endforeach
     </tbody>
