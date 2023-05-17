b7c37d00bf04173266a5d7e2fd839098bf683e77
Rafael Pianaro <rafaelpianaro@gmail.com>
2023-05-17 10:55:24 -0300

    add: partial and components for alert msgs

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,14 +1,7 @@
 <h1>Novo Suporte</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert/>
 
 <form action="{{ route('supports.store') }}" method="post" enctype="multipart/form" name="">
-    @csrf()
-    <input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') }}">
-    <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form')
 </form>
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,15 +1,10 @@
 <h1>Suporte {{ $support->id }}</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert/>
 
 <form action="{{ route('supports.update', $support->id) }}" method="post">
-    @csrf()
     @method('put')
-    <input type="text" name="subject" placeholder="Assunto" value="{{ $support->subject }}">
-    <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ $support->body }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', [
+        'support' => $support
+    ])
 </form>
